1f52322f3c64e173b0092d9ffffeca39d68300de
Abanti Ghosh <ABANTIGHOSH01@GMAIL.COM>
2025-11-22 20:10:31 +0530

    Revert "Resolved css & assets imports."

--- client/src/pages/student/Navbar.jsx
+++ client/src/pages/student/Navbar.jsx
@@ -1,37 +1,40 @@
-import React from "react";
-import { assets } from "../../assets/assets"; // Assets import
-import logo from "../../assets/logo.svg";
-import { Link, useLocation } from "react-router-dom";
+import React from 'react';
+import * as assets from '../../assets/assets';
+import logo from '../../assets/logo.svg';
+import { Link, useLocation } from 'react-router-dom';
 
 const Navbar = () => {
   const location = useLocation();
-  const isCourseListPage = location.pathname.includes("/course-list");
+  const isCourseListPage = location.pathname.includes('/course-list');
 
   return (
-    <div
-      className={`flex items-center justify-between px-4 sm:px-10 md:px-14 lg:px-36 border-b border-gray-500 py-4 ${
-        isCourseListPage ? "bg-white" : "bg-cyan-100/70"
-      }`}
-    >
-      <img src={logo} alt="Logo" className="w-28 lg:w-32 cursor-pointer" />
+    <div className={`flex items-center justify-between px-4 sm:px-10 md:px-14 lg:px-36 border-b border-gray-500 py-4 ${isCourseListPage ? 'bg-white' : 'bg-cyan-100/70'}`}>
+      <img
+      src={logo}
+      alt="Logo"
+      className="w-28 lg:w-32 cursor-pointer"
+    />
       <div className="hidden md:flex items-center gap-5 text-gray-500">
-        <div className="flex items-center gap-5">
-          <button>Become Educator</button> |
-          <Link to="/my-enrollments">My Enrollments</Link>
+        <div className='flex items-center gap-5'>
+        <button>Become Educator</button> |
+          <Link to="/my-enrollments">
+            My Enrollments
+          </Link>
         </div>
-        <button className="bg-blue-600 text-white px-5 py-2 rounded-full ">
+        <button className='bg-blue-600 text-white px-5 py-2 rounded-full '>
           Create Account
         </button>
       </div>
-      {/*For Phone Screens */}
-      <div className="md:hidden flex items-center gap-2 sm:gap-5 text-gray-500">
+  {/*For Phone Screens */}
+      <div classname='md:hidden flex items-center gap-2 sm:gap-5 text-gray-500'>
         <div>
           <button>Become Educator</button> |
-          <Link to="/my-enrollments">My Enrollments</Link>
+          <Link to="/my-enrollments">
+            My Enrollments
+          </Link>
         </div>
         <button>
-          {/* User icon usage */}
-          <img src={assets.user_icon} alt="" />
+          <img src={user_icon} alt="" />
         </button>
       </div>
     </div>
