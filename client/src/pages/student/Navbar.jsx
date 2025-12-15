deed3e8ba75984927f435e1f9539a7ad838ca28f
Anjel-cmd <abantighosh01@gmail.com>
2025-12-15 19:31:31 +0530

    Frontend Completed my enroll ment , students enrolled , Add courses Page

--- client/src/pages/student/Navbar.jsx
+++ client/src/pages/student/Navbar.jsx
@@ -43,7 +43,7 @@ const Navbar = () => {
           <UserButton />
         ) : (
           <button
-            // onClick={() => openSignIn()}
+            onClick={() => openSignIn()}
             className="bg-blue-600 text-white px-5 py-2 rounded-full "
           >
             Create Account
